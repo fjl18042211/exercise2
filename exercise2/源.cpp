934892aa14cdd2e60fb9fccb95e34e664bdd9f40
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-03-27 14:21:03 +0800

    week5ass2

--- exercise2/源.cpp
+++ exercise2/源.cpp
@@ -1,43 +1,38 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include <opencv.hpp>
+#include <stdlib.h>
 using namespace cv;
 using namespace std;
-
 int main()
 {
-	cv::Mat binaryMat;
-	cv::Mat labelMat;
-	cv::Mat statsMat;
-	cv::Mat centrMat;
-	cv::Mat srcMat = imread("D:\\4.png", 0);
-	int font_face = cv::FONT_HERSHEY_COMPLEX;
-	double font_scale = 1.5;
-	int thickness = 1;
-	int baseline;
-	int elementSize = 5;
-	cv::threshold(srcMat, binaryMat, 0, 255, THRESH_OTSU);
-	int nComp = cv::connectedComponentsWithStats(binaryMat,
-		labelMat,
-		statsMat,
-		centrMat,
-		8,
-		CV_32S);
-	cout << "the total of connected Components = " << nComp - 1 << endl;
-
-	for (int i = 1; i < nComp; i++)
+	VideoCapture cap;
+	cap.open(0);
+	if (!cap.isOpened())
 	{
-		char num[10];
-		sprintf_s(num, "%d", i);
-		Rect bndbox;
-		bndbox.x = statsMat.at<int>(i, 0);
-		bndbox.y = statsMat.at<int>(i, 1);
-		bndbox.width = statsMat.at<int>(i, 2);
-		bndbox.height = statsMat.at<int>(i, 3);
-		rectangle(binaryMat, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
-		cv::putText(binaryMat, num, Point(bndbox.x, bndbox.y), font_face, 1, cv::Scalar(0, 255, 255), thickness, 8, 0);
+		std::cout << "不能打开视频文件" << std::endl;
+		return -1;
 	}
-	imshow("binaryMat", binaryMat);
-	imshow("frame", srcMat);
-	waitKey(0);
+	double fps = cap.get(CAP_PROP_FPS);
+	std::cout << "fps" << fps << std::endl;
+	while (1)
+	{
+		cv::Mat frame;
+		cv::Mat dst;
+		bool rSucess = cap.read(frame);
+		if (!rSucess)
+		{
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else
+		{
+			cv::blur(frame, dst, Size(3,3));
+			cv::imshow("dst", dst);
+
+		}
+
+	}
+	waitKey(30);
 	return 0;
+
 }
