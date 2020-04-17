9105728e2d5dc942cf53d18ff1c0466e58a97883
fjl18042211 <61268428+fjl18042211@users.noreply.github.com>
2020-04-17 16:15:55 +0800

    week8ass2

--- exercise2/源.cpp
+++ exercise2/源.cpp
@@ -1,32 +1,46 @@
-#include <iostream>
-#include <opencv.hpp>
-#include <stdlib.h>
-using namespace cv;
-using namespace std;
-int main()
-{
-	cv::Mat canny;
-	cv::Mat srcMat = cv::imread("D:\\metal-part-distorted-03.png", 1);
-	if (srcMat.empty()) return-1;
-	Canny(srcMat, canny, 60, 180);
-	cv::imshow("canny", canny);
-	std::vector<cv::Vec2f>lines;
-	cv::HoughLines(canny, lines, 1, CV_PI / 180,100);
-	std::vector<cv::Vec2f>::iterator it = lines.begin();
-	for (; it != lines.end(); ++it) {
-		float rho = (*it)[0], theta = (*it)[1];
-		cv::Point pt1, pt2;
-		double a = cos(theta);
-		double b = sin(theta);
-		double x0 = a * rho;
-		double y0 = b * rho;
-		pt1.x = cv::saturate_cast<int>(x0 + 1000 * (-b));
-		pt1.y = cv::saturate_cast<int>(y0 + 1000 * (a));
-		pt2.x = cv::saturate_cast<int>(x0 - 1000 * (-b));
-		pt2.y = cv::saturate_cast<int>(y0 -1000 * (a));
-		cv::line(srcMat, pt1, pt2, cv::Scalar(0, 0, 255), 1, CV_AA);
-	}
-	imshow("src", srcMat);
-	cv::waitKey(0);
+ #include <opencv.hpp>
+ #include <math.h>
 
-}
+
+ using namespace cv;
+ using namespace std;
+
+
+ int main()
+ {
+	 	Mat srcMat = imread("D:\\die_on_chip.png", 0);
+	 	Mat dspMat = imread("D:\\die_on_chip.png");
+	 	Mat binaryMat;
+		 	threshold(srcMat, binaryMat, 150, 255, THRESH_OTSU);
+		 	Mat element = getStructuringElement(MORPH_RECT, Size(8, 9));
+	 	morphologyEx(binaryMat, binaryMat, MORPH_OPEN, element);
+		 	vector<vector<Point>> contours;
+	 	findContours(binaryMat, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE, Point());
+		cout << "num=" << contours.size() << endl;
+		 	float rate[100];
+		 	for (int i = 0; i < contours.size(); i++)
+		 	{
+		 		RotatedRect rect = minAreaRect(contours[i]);
+		 		Point2f P[4];
+			rect.points(P);
+		 	
+					float Y = sqrt((P[0].y - P[1].y) * (P[0].y - P[1].y) + (P[0].x - P[1].x) * (P[0].x - P[1].x));
+		 		float X = sqrt((P[1].y - P[2].y) * (P[1].y - P[2].y) + (P[1].x - P[2].x) * (P[1].x - P[2].x));
+				rate[i] = X / Y;
+		 		cout << "num=" << i << "rate of rect =" << rate[i] << endl;
+		 		if ((rate[i] >= 0.8) and (rate[i] <= 1.0))
+			 		{
+						for (int j = 0; j <= 3; j++)
+						{
+				 				line(dspMat, P[j], P[(j + 1) % 4], Scalar(255), 1);
+							}
+					}
+			}
+	
+
+	    imshow("srcMat", srcMat);
+	 	imshow("dspMat", dspMat);
+	 	imshow("binaryMat", binaryMat);
+	 	waitKey(0);
+	 	return 0;
+	 }
